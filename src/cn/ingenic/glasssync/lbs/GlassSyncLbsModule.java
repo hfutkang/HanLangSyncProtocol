c7de7678d549beb6931f9537e5bbfe028aed7131
jim <jqian@ingenic.cn>
2014-05-09 17:50:49 +0800

    modify the glass sync side due to the format change of mobile gps uploading.
    Change-Id: I5cb6c213e2f4afcf2f266136dd7191c06b9e39d6

--- src/cn/ingenic/glasssync/lbs/GlassSyncLbsModule.java
+++ src/cn/ingenic/glasssync/lbs/GlassSyncLbsModule.java
@@ -10,9 +10,9 @@ public class GlassSyncLbsModule extends SyncModule {
     private static final String TAG = "GlassSyncLbsModule";
     private static final String LETAG = "GSLBSMD";
 
-    private static final String FLT1 = "FLT1";
-    private static final String FLT2 = "FLT2";
-    private static final String FLT3 = "FLT3";
+    private static final String LATITUDE = "latitude";
+    private static final String LONGITUDE = "longitude";
+    private static final String BEARING = "bearing";
 
     private Context mContext;
     private static GlassSyncLbsModule sInstance;
@@ -20,38 +20,38 @@ public class GlassSyncLbsModule extends SyncModule {
 	MockLocation mMockLocation;
 
     private GlassSyncLbsModule(Context context){
-	super(LETAG, context);
-	mContext = context;
-	mMockLocation = new MockLocation(MockLocation.ProviderType.GPS, context);
-	mMockLocation.init();
+		super(LETAG, context);
+		mContext = context;
+		mMockLocation = new MockLocation(MockLocation.ProviderType.GPS, context);
+		mMockLocation.init();
     }
 
     public static GlassSyncLbsModule getInstance(Context c) {
-	if (null == sInstance)
-	    sInstance = new GlassSyncLbsModule(c);
-	return sInstance;
+		if (null == sInstance)
+			sInstance = new GlassSyncLbsModule(c);
+		return sInstance;
     }
 
     @Override
-    protected void onCreate() {
+	protected void onCreate() {
     }
 
     @Override
-    protected void onRetrive(SyncData data) {
-	Log.e(TAG, "onRetrive");
+	protected void onRetrive(SyncData data) {
+		Log.e(TAG, "onRetrive");
 
-	float f1 = data.getFloat(FLT1);
-	float f2 = data.getFloat(FLT2);
-	float f3 = data.getFloat(FLT3);
+		Double lat = data.getDouble(LATITUDE);
+		Double lon = data.getDouble(LONGITUDE);
+		float bearing = data.getFloat(BEARING);
 
-	MockLocationInfo info = new MockLocationInfo();
-	info.setLatitude((double)f1);
-	info.setLongitude((double)f2);
-	info.setBearing((float)f3);
-	info.setTime(java.lang.System.currentTimeMillis());
-	
-	mMockLocation.sendLocation(info);
-	
-	Log.e(TAG, "f1:" + f1 + " f2:" + f2 + " f3:" + f3);
+		MockLocationInfo info = new MockLocationInfo();
+		info.setLatitude(lat);
+		info.setLongitude(lon);
+		info.setBearing(bearing);
+		info.setTime(java.lang.System.currentTimeMillis());
+		
+		mMockLocation.sendLocation(info);
+		
+		Log.e(TAG, "lat:" + lat + " lon:" + lon + " bearing:" + bearing);
     }
 }
