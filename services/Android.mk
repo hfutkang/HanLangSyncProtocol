a8686a437a06ae166a0fbd627b68c5bf608df288
jim <jqian@ingenic.cn>
2014-06-26 16:14:31 +0800

    build glass sync aidl part as jar to share with other packages.
    Change-Id: Ic802ccd8fc04c29b007d8e38766180fe8ca89ab1

--- services/Android.mk
+++ services/Android.mk
@@ -1,5 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
+###############################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := GlassSyncAIDL
+LOCAL_SRC_FILES := $(call all-java-files-under, src/cn/ingenic/glasssync/services)
+
+LOCAL_AIDL_INCLUDES := src/cn/ingenic/glasssync/services
+
+include $(BUILD_JAVA_LIBRARY)
+
+###############################################
+
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := cn.ingenic.glasssync.services
