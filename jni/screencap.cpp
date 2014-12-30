54d5224fd9485f6b920b838fdeed0bbd4d8431c6
xqliang <xqliang@ingenic.cn>
2014-12-30 15:17:04 +0800

    use mxu instructions opt rgb565toyuv420sp

--- jni/screencap.cpp
+++ jni/screencap.cpp
@@ -50,7 +50,7 @@ using namespace android;
 #include <sys/time.h>
 #ifdef TIME_COST_TEST
 // Returns current time in microseconds
-static unsigned int GetTimer(void){
+static unsigned int GetTimer(void) {
     struct timeval tv;
     gettimeofday(&tv,NULL);
     
@@ -58,8 +58,6 @@ static unsigned int GetTimer(void){
 }
 #endif
 
-
-//unsigned char *YUVdest, *YUV;
 static int init_rgb_tab = 0;
 int frameNum = 0;
 int fb_width, fb_height;
@@ -69,9 +67,7 @@ unsigned int costtime = 0;
 unsigned int s, f;
 
 static status_t vinfoToPixelFormat(const fb_var_screeninfo& vinfo,
-        uint32_t* bytespp, uint32_t* f)
-{
-
+        uint32_t* bytespp, uint32_t* f) {
     switch (vinfo.bits_per_pixel) {
         case 16:
             *f = PIXEL_FORMAT_RGB_565;
@@ -115,6 +111,7 @@ int *getfb(void) {
 	    uint32_t bytespp;
 	    if (vinfoToPixelFormat(vinfo, &bytespp, &f) == NO_ERROR) {
 		bits_per_pixel = vinfo.bits_per_pixel;
+		ALOGE("bits_per_pixel = %d,bytespp=%d",bits_per_pixel,bytespp);
 	        size_t offset = (vinfo.xoffset + vinfo.yoffset*vinfo.xres) * bytespp;
 		if (ioctl(fb, FBIOGET_FSCREENINFO, &finfo) == 0) {
 		  //ALOGE("%d %s(), finfo.line_length=%d", __LINE__, __FUNCTION__, finfo.line_length);
@@ -138,7 +135,7 @@ int *getfb(void) {
     return new_rgbx_base;
 }
 
-void RGB565_to_YUV420SP_c(
+void RGB565_to_YUV420SP_c (
     unsigned char *y_dst,
     unsigned char *uv_dst,
     unsigned char *rgb_src,
@@ -173,9 +170,7 @@ void RGB565_to_YUV420SP_c(
             Y = ((66 * R) + (129 * G) + (25 * B) + 128);
             Y = Y >> 8;
             Y += 16;
-
             pDstY[yIndex++] = (unsigned char)Y;
-
             if ((j % 2) == 0 && (i % 2) == 0) {
                 U = ((-38 * R) - (74 * G) + (112 * B) + 128);
                 U = U >> 8;
@@ -191,7 +186,8 @@ void RGB565_to_YUV420SP_c(
     }
 }
 
-void RGB565_to_YUV420SP_CHANGED(
+
+void RGB565_to_YUV420SP_CHANGED (
     unsigned char *y_dst,
     unsigned char *uv_dst,
     unsigned char *rgb_src,
@@ -199,47 +195,68 @@ void RGB565_to_YUV420SP_CHANGED(
     int height)
 {
     unsigned int i, j;
-    unsigned int tmp;
-
-    unsigned int R, G, B;
-    unsigned int Y, U, V;
-
+    unsigned short int *src;
     unsigned short int *pSrc = (unsigned short int *)rgb_src;
-    unsigned char *pDstY = (unsigned char *)y_dst;
-    unsigned char *pDstUV = (unsigned char *)uv_dst;
+    unsigned int *dest = (unsigned int *)y_dst;
+    unsigned int *uvdest = (unsigned int *)uv_dst;
 
-    unsigned int yIndex = 0;
-    unsigned int uvIndex = 0;
+    dest -= 1;
+    uvdest -= 1;
 
     for (j = 0; j < height; j+=2) {
-        for (i = 0; i < width; i+=2) {
-            tmp = pSrc[j * fb_width + i];
+        src = pSrc + j * fb_width - 2;
+	for (i = 0; i < width; i+=8) {
+  	    S16LDI(xr1, src, 4, 0);  
+	    S16LDI(xr1, src, 4, 1); //xr1: R2G2B2, R1G1B1
+	    S16LDI(xr2, src, 4, 0);  
+	    S16LDI(xr2, src, 4, 1); //xr2: R4G4B4, R3G3B3
 
-            R = (tmp & 0x0000F800) >> 11;
-            R = R * 8;
-            G = (tmp & 0x000007E0) >> 5;
-            G = G * 4;
-            B = (tmp & 0x0000001F);
-            B = B * 8;
+	    Q16SAR(xr3,xr1,xr2,xr4,11);  //xr3:R2,R1 , xr4:R4,R3
+	    Q16SLL(xr5,xr1,xr2,xr6,5);
+	    Q16SAR(xr7,xr5,xr6,xr8,10);  //xr7:G2,G1, XR8:G4,G3
 
-            Y = ((66 * R) + (129 * G) + (25 * B) + 128);
-            Y = Y >> 8;
-            Y += 16;
+	    Q16SLL(xr5,xr1,xr2,xr6,11);
+	    Q16SAR(xr5,xr5,xr6,xr6,11);  //xr5:B2,B1, xr6:B4,B3
 
-            pDstY[yIndex++] = (unsigned char)Y;
+	    Q16SLL(xr3,xr3,xr4,xr4,3);   //xr3:R2<<3,R1<<3
+	    Q16SLL(xr7,xr7,xr8,xr8,2);   //xr7:G2<<2,G1<<2
+	    Q16SLL(xr5,xr5,xr6,xr6,3);
 
-            if ((j % 2) == 0 && (i % 2) == 0) {
-                U = ((-38 * R) - (74 * G) + (112 * B) + 128);
-                U = U >> 8;
-                U += 128;
-                V = ((112 * R) - (94 * G) - (18 * B) + 128);
-                V = V >> 8;
-                V += 128;
+	    S32SFL(xr0,xr4,xr3,xr3,1);  //xr3:R4R3R2R1
+	    S32SFL(xr0,xr8,xr7,xr7,1);  //xr7:G4G3G2G1
+	    S32SFL(xr0,xr6,xr5,xr5,1);  //xr5:B4B3B2B1
 
-                pDstUV[uvIndex++] = (unsigned char)U;
-                pDstUV[uvIndex++] = (unsigned char)V;
-            }
-        }
+	    Q8MUL(xr1,xr3,xr15,xr2);  //xr1:R4*66, R2*66 , xr2:R3*66, R1*66
+	    Q8MAC_AA(xr1,xr13,xr7,xr2); //xr1:R4*66+G4*129,R2*66+G2*129  xr2: R3*66+G3*129,R1*66+G1*129
+	    Q8MAC_AA(xr1,xr5,xr12,xr2); //xr1:R4*66+G4*129+B4*25,R2*66+G2*129+B2*25  xr2: R3*66+G3*129+B3*25,R1*66+G1*129+B1*25
+	    Q8MAC_AA(xr1,xr11,xr14,xr2); //xr1:R4*66+G4*129+B4*25+80,R2*66+G2*129+B2*25+80  xr2: R3*66+G3*129+B3*25+80,R1*66+G1*129+B1*25+80
+
+	    S32I2M(xr6, 0x00100010); //U 1,1 112,112
+	    Q16SAR(xr1,xr1,xr2,xr2,8); //>>8
+	    Q16ACCM_AA(xr1,xr6,xr6,xr2); //Y+16
+
+	    S32SFL(xr0,xr1,xr2,xr1,1);
+	    S32SDI(xr1, dest, 0x4);
+	    
+	    if ((j % 4) == 0) {
+	        S32I2M(xr8, 0x12125E5E); //V 18, 18, 94,94
+		S32SFL(xr0,xr3,xr7,xr3,1);  //xr3:R3R1G3G1
+		S32SFL(xr0,xr14,xr5,xr5,1);  //xr9:8080B3B1
+		
+		Q8MUL(xr1,xr5,xr9,xr2); //xr1:80*1,80*1, xr2:B3*112,B1*112
+		Q8MAC_SS(xr1,xr3,xr10,xr2); //xr1:80-R3*38,80-R1*38, xr2:B3*112-G3*74,B1*112-G1*74
+		
+		S32SFL(xr5,xr5,xr3,xr3,3);  //xr5:8080R3R1, xr3:B3B1G3G1
+		Q8MUL(xr4,xr5,xr9,xr6); //xr4:80*1,80*1,xr5:R3*112, R1*112
+		Q8MAC_SS(xr4,xr3,xr8,xr6); //xr4:80*1-B3*18,80*1-B1*18, xr5:R3*112-G3*94, R1*112-G1*94
+		Q16ACCM_AA(xr1,xr2,xr6,xr4); //xr1:80-R3*38+B3*112-G3*74(U2),80-R1*38+B1*112-G1*74(U1)  //xr4:80-B3*18+R3*112-G3*94,80-B1*18+R1*112-G1*94 
+		Q16SAR(xr1,xr1,xr4,xr4,8); //>>8
+		Q16ACCM_AA(xr1,xr14,xr14,xr4); //UV+128
+		S32SFL(xr0,xr4,xr1,xr1,2);
+		S32SDI(xr1, uvdest,4);
+	    }
+
+	}
     }
 }
 
@@ -287,6 +304,79 @@ void ARGB8888_to_YUV420SP_c(
                 pDstUV[uvIndex++] = (unsigned char)U;
                 pDstUV[uvIndex++] = (unsigned char)V;
             }
+        }
+    }
+}
+
+
+void RGB565_to_YUV420SP(
+    unsigned char *y_dst,
+    unsigned char *uv_dst,
+    unsigned char *rgb_src,
+    int width,
+    int height)
+{
+    unsigned int i, j;
+    unsigned short int *src;
+    unsigned short int *pSrc = (unsigned short int *)rgb_src;
+    unsigned int *dest = (unsigned int *)y_dst;
+    unsigned int *uvdest = (unsigned int *)uv_dst;
+
+    dest -= 1;
+    uvdest -= 1;
+
+    for (j = 0; j < height; j++) {
+        src = pSrc + j * fb_width - 1;
+        for (i = 0; i < width; i+=4) {
+	    S16LDI(xr1, src, 2, 0);  
+	    S16LDI(xr1, src, 2, 1); //xr1: R2G2B2, R1G1B1
+	    S16LDI(xr2, src, 2, 0);  
+	    S16LDI(xr2, src, 2, 1); //xr2: R4G4B4, R3G3B3
+
+	    Q16SAR(xr3,xr1,xr2,xr4,11);  //xr3:R2,R1 , xr4:R4,R3
+	    Q16SLL(xr5,xr1,xr2,xr6,5);
+	    Q16SAR(xr7,xr5,xr6,xr8,10);  //xr7:G2,G1, XR8:G4,G3
+
+	    Q16SLL(xr5,xr1,xr2,xr6,11);
+	    Q16SAR(xr5,xr5,xr6,xr6,11);  //xr5:B2,B1, xr6:B4,B3
+
+	    Q16SLL(xr3,xr3,xr4,xr4,3);   //xr3:R2<<3,R1<<3
+	    Q16SLL(xr7,xr7,xr8,xr8,2);   //xr7:G2<<2,G1<<2
+	    Q16SLL(xr5,xr5,xr6,xr6,3);
+
+	    S32SFL(xr0,xr4,xr3,xr3,1);  //xr3:R4R3R2R1
+	    S32SFL(xr0,xr8,xr7,xr7,1);  //xr7:G4G3G2G1
+	    S32SFL(xr0,xr6,xr5,xr5,1);  //xr5:B4B3B2B1
+
+	    Q8MUL(xr1,xr3,xr15,xr2);  //xr1:R4*66, R2*66 , xr2:R3*66, R1*66
+	    Q8MAC_AA(xr1,xr13,xr7,xr2); //xr1:R4*66+G4*129,R2*66+G2*129  xr2: R3*66+G3*129,R1*66+G1*129
+	    Q8MAC_AA(xr1,xr5,xr12,xr2); //xr1:R4*66+G4*129+B4*25,R2*66+G2*129+B2*25  xr2: R3*66+G3*129+B3*25,R1*66+G1*129+B1*25
+	    Q8MAC_AA(xr1,xr11,xr14,xr2); //xr1:R4*66+G4*129+B4*25+80,R2*66+G2*129+B2*25+80  xr2: R3*66+G3*129+B3*25+80,R1*66+G1*129+B1*25+80
+
+	    S32I2M(xr6, 0x00100010); //U 1,1 112,112
+	    Q16SAR(xr1,xr1,xr2,xr2,8); //>>8
+	    Q16ACCM_AA(xr1,xr6,xr6,xr2); //Y+16
+
+	    S32SFL(xr0,xr1,xr2,xr1,1);
+	    S32SDI(xr1, dest, 0x4);
+
+	    if ((j % 2) == 0) {
+	        S32I2M(xr8, 0x12125E5E); //V 18, 18, 94,94
+		S32SFL(xr0,xr3,xr7,xr3,1);  //xr3:R3R1G3G1
+		S32SFL(xr0,xr14,xr5,xr5,1);  //xr9:8080B3B1
+		
+		Q8MUL(xr1,xr5,xr9,xr2); //xr1:80*1,80*1, xr2:B3*112,B1*112
+		Q8MAC_SS(xr1,xr3,xr10,xr2); //xr1:80-R3*38,80-R1*38, xr2:B3*112-G3*74,B1*112-G1*74
+		
+		S32SFL(xr5,xr5,xr3,xr3,3);  //xr5:8080R3R1, xr3:B3B1G3G1
+		Q8MUL(xr4,xr5,xr9,xr6); //xr4:80*1,80*1,xr5:R3*112, R1*112
+		Q8MAC_SS(xr4,xr3,xr8,xr6); //xr4:80*1-B3*18,80*1-B1*18, xr5:R3*112-G3*94, R1*112-G1*94
+		Q16ACCM_AA(xr1,xr2,xr6,xr4); //xr1:80-R3*38+B3*112-G3*74(U2),80-R1*38+B1*112-G1*74(U1)  //xr4:80-B3*18+R3*112-G3*94,80-B1*18+R1*112-G1*94 
+		Q16SAR(xr1,xr1,xr4,xr4,8); //>>8
+		Q16ACCM_AA(xr1,xr14,xr14,xr4); //UV+128
+		S32SFL(xr0,xr4,xr1,xr1,2);
+		S32SDI(xr1, uvdest,4);
+	    }
         }
     }
 }
@@ -463,151 +553,156 @@ inline void ARGB8888_to_YUV420SP_CHANGED(
 
  
 static void get_framebuffer_data(JNIEnv* env, jobject thiz,jbyteArray data) {
-  jbyte* readBuff = NULL;
-  readBuff = (jbyte *)env->GetByteArrayElements(data, NULL);
+    jbyte* readBuff = NULL;
+    readBuff = (jbyte *)env->GetByteArrayElements(data, NULL);
 
-  if (readBuff == NULL) {
-    ALOGE("Error retrieving destination for data");
-    return;
-  }
+    if (readBuff == NULL) {
+        ALOGE("Error retrieving destination for data");
+	return;
+    }
 
 
 #ifdef TIME_COST_TEST
-  unsigned int pre_time1 = GetTimer();
+    unsigned int pre_time1 = GetTimer();
 #endif
 
-  int *p = NULL;
-  p = getfb();
+    int *p = NULL;
+    p = getfb();
 
-  if (p == NULL) {
-    ALOGE("framebuffer is null");
-    return;
-  }
+    if (p == NULL) {
+        ALOGE("framebuffer is null");
+	return;
+    }
 
 #ifdef TIME_COST_TESTN
-  frameNum++;
-  unsigned int time1 = GetTimer() - pre_time1;
-  costtime += time1;
-  ALOGE("getfb costTime = %u us", time1);
-  ALOGE("average getfb costTime = %u us, frameNum = %d", costtime/frameNum,frameNum);
+    frameNum++;
+    unsigned int time1 = GetTimer() - pre_time1;
+    costtime += time1;
+    ALOGE("getfb costTime = %u us", time1);
+    ALOGE("average getfb costTime = %u us, frameNum = %d", costtime/frameNum,frameNum);
 #endif
 
-
-  unsigned char *rgb_src = (unsigned char *)p;
-  unsigned char *yuv_dst = (unsigned char *)readBuff;
-  unsigned char *uv_dst = yuv_dst + changed_width * changed_height;
-
+    
+    unsigned char *rgb_src = (unsigned char *)p;
+    unsigned char *yuv_dst = (unsigned char *)readBuff;
+    unsigned char *uv_dst = yuv_dst + changed_width * changed_height;
+    
 #ifdef TIME_COST_TESTN
-  unsigned int pre_time2 = GetTimer();
+    unsigned int pre_time2 = GetTimer();
 #endif
 
-  if (bits_per_pixel == 32) {
-      ALOGE("ARGB8888");
-      if (changed_width == fb_width) {
-	  ARGB8888_to_YUV420SP(yuv_dst, uv_dst, rgb_src, fb_width, fb_height);
-      }else{
-	  ARGB8888_to_YUV420SP_CHANGED(yuv_dst, uv_dst, rgb_src, changed_width*2, changed_height*2);
-      }
-  }else if (bits_per_pixel == 16){
-      if (changed_width == fb_width) {
-	  RGB565_to_YUV420SP_c(yuv_dst, uv_dst, rgb_src, fb_width, fb_height);
-      }else{
-	  RGB565_to_YUV420SP_CHANGED(yuv_dst, uv_dst, rgb_src, changed_width*2, changed_height*2);
-      }
-  }else{
-      ALOGE("neither ARGB8888 or RGB565, unsupported");
-  }
+    if (bits_per_pixel == 32) {
+        S32I2M(xr15, 0x19814201); //Y bgr80
+	S32I2M(xr12, 0x704A2601); //U bgr80
+	S32I2M(xr11, 0x125E7001); //V bgr80
+	S32I2M(xr14, 0x80000000);
+	S32I2M(xr13, 0x00100010); //for Y
+	if (changed_width == fb_width) {
+	    ARGB8888_to_YUV420SP(yuv_dst, uv_dst, rgb_src, fb_width, fb_height);
+	}else{
+	    ARGB8888_to_YUV420SP_CHANGED(yuv_dst, uv_dst, rgb_src, changed_width*2, changed_height*2);
+	}
+    }else if (bits_per_pixel == 16){
+        S32I2M(xr15,0x42424242);
+	S32I2M(xr14, 0x80808080);
+	S32I2M(xr13,0x81818181);
+	S32I2M(xr12,0x19191919);
+	S32I2M(xr11,0x01010101);
+	S32I2M(xr10, 0x26264A4A); //U 74,74,38,38
+	S32I2M(xr9, 0x01017070); //U 1,1 112,112
+	if (changed_width == fb_width) {
+	    RGB565_to_YUV420SP(yuv_dst, uv_dst, rgb_src, fb_width, fb_height);
+	}else{
+	    RGB565_to_YUV420SP_CHANGED(yuv_dst, uv_dst, rgb_src, changed_width*2, changed_height*2);
+	}
+    }else{
+        ALOGE("neither ARGB8888 or RGB565, unsupported");
+    }
 
 #ifdef TIME_COST_TEST
-  unsigned int time2 = GetTimer() - pre_time1;
-  frameNum++;
-  costtime += time2;
-  ALOGE("rgbtoyuv costTime = %u us", time2);
-  ALOGE("average getfb costTime = %u us, frameNum = %d", costtime/frameNum,frameNum);
+    unsigned int time2 = GetTimer() - pre_time1;
+    frameNum++;
+    costtime += time2;
+    ALOGE("rgbtoyuv costTime = %u us", time2);
+    ALOGE("average getfb costTime = %u us, frameNum = %d", costtime/frameNum,frameNum);
 #endif
 
+    
+    if (mapbase != MAP_FAILED) {
+        munmap((void *)mapbase, mapsize);
+    }
 
-  if (mapbase != MAP_FAILED) {
-    munmap((void *)mapbase, mapsize);
-  }
-
-  env->ReleaseByteArrayElements(data, readBuff, 0);
+    env->ReleaseByteArrayElements(data, readBuff, 0);
 #ifdef TIME_COST_TESTN
-  unsigned int time1 = GetTimer() - pre_time1;
-  ALOGE("rgbtoyuv costTime = %u us", time1);
+    unsigned int time1 = GetTimer() - pre_time1;
+    ALOGE("rgbtoyuv costTime = %u us", time1);
 #endif
 }
 
 
 static jint get_frame_width(JNIEnv* env, jobject thiz) {
-  return (jint)changed_width;
+    return (jint)changed_width;
 }
 
 static jint  get_frame_height(JNIEnv* env, jobject thiz) {
-  return (jint)changed_height;
+    return (jint)changed_height;
 }
 
 
 static void get_picture_size(JNIEnv* env, jobject thiz) {
-  unsigned int f1, bytespp;
-  const char* fbpath = "/dev/graphics/fb0";
-  frameNum = 0;
-  int fb = open(fbpath, O_RDONLY);
+    unsigned int f1, bytespp;
+    const char* fbpath = "/dev/graphics/fb0";
+    frameNum = 0;
+    int fb = open(fbpath, O_RDONLY);
 
-  costtime = 0;
-  if (fb >= 0) {
-     struct fb_var_screeninfo vinfo;
-     struct fb_fix_screeninfo finfo;
-     if (ioctl(fb, FBIOGET_VSCREENINFO, &vinfo) == 0) {
-       uint32_t bytespp;
-       if (vinfoToPixelFormat(vinfo, &bytespp, &f1) == NO_ERROR) {
-	 size_t offset = (vinfo.xoffset + vinfo.yoffset*vinfo.xres) * bytespp;
-	 fb_width = vinfo.xres;
-	 fb_height = vinfo.yres;
-	 if (ioctl(fb, FBIOGET_FSCREENINFO, &finfo) == 0) {
-	   s = finfo.line_length/bytespp;
-	   frame_size = fb_height*s*bytespp;
-	 }else {
-	   s = vinfo.xres;
-	   frame_size = fb_width*fb_height*bytespp;
-	 }
-	 map_offset = (vinfo.xoffset + vinfo.yoffset*s) * bytespp;
-	 mapsize = map_offset + frame_size;
-	 ALOGE("get_picture map_offset=%d,mapsize=%d",map_offset,mapsize);
-       }else{
-	 fb_width = default_frame_width;
-	 fb_height = default_frame_height;
-       }
-     }
-  }else{
-    fb_width = default_frame_width;
-    fb_height = default_frame_height;
-  }
+    costtime = 0;
+    if (fb >= 0) {
+        struct fb_var_screeninfo vinfo;
+	struct fb_fix_screeninfo finfo;
+	if (ioctl(fb, FBIOGET_VSCREENINFO, &vinfo) == 0) {
+	    uint32_t bytespp;
+	    if (vinfoToPixelFormat(vinfo, &bytespp, &f1) == NO_ERROR) {
+	        size_t offset = (vinfo.xoffset + vinfo.yoffset*vinfo.xres) * bytespp;
+		fb_width = vinfo.xres;
+		fb_height = vinfo.yres;
+		if (ioctl(fb, FBIOGET_FSCREENINFO, &finfo) == 0) {
+		    s = finfo.line_length/bytespp;
+		    frame_size = fb_height*s*bytespp;
+		}else {
+		    s = vinfo.xres;
+		    frame_size = fb_width*fb_height*bytespp;
+		}
+		map_offset = (vinfo.xoffset + vinfo.yoffset*s) * bytespp;
+		mapsize = map_offset + frame_size;
+	    }else{
+	        fb_width = default_frame_width;
+		fb_height = default_frame_height;
+	    }
+	}
+    }else{
+        fb_width = default_frame_width;
+	fb_height = default_frame_height;
+    }
 
-  if (fb_width > 400) {
-    changed_width = fb_width / 2;
-    changed_height = fb_height / 2;
-    if (changed_width % 16) changed_width = changed_width / 16 * 16;  
-    if (changed_height % 16) changed_height = changed_height / 16 * 16;  
-  }else{
-    changed_width = fb_width;
-    changed_height = fb_height;
-    if (changed_width % 16) changed_width = changed_width / 16 * 16;  
-    if (changed_height % 16) changed_height = changed_height / 16 * 16;  
-  }
+    ALOGE("fb_width = %d,fb_height=%d",fb_width,fb_height);
+    if (fb_width > 400) {
+        changed_width = fb_width / 2;
+	changed_height = fb_height / 2;
+	if (changed_width % 16) changed_width = changed_width / 16 * 16;  
+	if (changed_height % 16) changed_height = changed_height / 16 * 16;  
+    }else{
+        changed_width = fb_width;
+	changed_height = fb_height;
+	if (changed_width % 16) changed_width = changed_width / 16 * 16;  
+	if (changed_height % 16) changed_height = changed_height / 16 * 16;  
+    }
 
 
-  close(fb);
-  if (init_rgb_tab == 0) {
-    S32I2M(xr16, 0x7);
-    init_rgb_tab = 1;
-
-    S32I2M(xr15, 0x19814201); //Y bgr80
-    S32I2M(xr12, 0x704A2601); //U bgr80
-    S32I2M(xr11, 0x125E7001); //V bgr80
-    S32I2M(xr14, 0x80000000);
-    S32I2M(xr13, 0x00100010); //for Y
-  }
+    close(fb);
+    if (init_rgb_tab == 0) {
+        S32I2M(xr16, 0x7);
+	init_rgb_tab = 1;
+    }
 }
 
    
@@ -622,34 +717,32 @@ static JNINativeMethod gMethods[] = {
 
 static int register_android_mmi(JNIEnv *env)
 {
-  return AndroidRuntime::registerNativeMethods(env, kClassPathName, gMethods, NELEM(gMethods));
+    return AndroidRuntime::registerNativeMethods(env, kClassPathName, gMethods, NELEM(gMethods));
 }
 
 
 jint JNI_OnLoad(JavaVM* vm, void* reserved)
 {
-	JNIEnv* env = NULL;
-	jint result = -1;
-
-	ALOGE("JNI_OnLoad\n");
-
-	if (vm->GetEnv((void**) &env, JNI_VERSION_1_4) != JNI_OK)
-	{
-	  ALOGE("ERROR: GetEnv failed\n");
-	  return result;
-	}
-	assert(env != NULL);
-
-	if (register_android_mmi(env) < 0)
-	{
-	  ALOGE("ERROR: colorspace changed native registration failed\n");
-	  return result;
-	}
-
-	result = JNI_VERSION_1_4;
-	
-	ALOGE("JNI_OnLoad end\n");
+    JNIEnv* env = NULL;
+    jint result = -1;
+    
+    ALOGE("JNI_OnLoad\n");
+    
+    if (vm->GetEnv((void**) &env, JNI_VERSION_1_4) != JNI_OK) {
+        ALOGE("ERROR: GetEnv failed\n");
 	return result;
+    }
+    assert(env != NULL);
+    
+    if (register_android_mmi(env) < 0) {
+        ALOGE("ERROR: colorspace changed native registration failed\n");
+	return result;
+    }
+
+    result = JNI_VERSION_1_4;
+	
+    ALOGE("JNI_OnLoad end\n");
+    return result;
 }
  
  
